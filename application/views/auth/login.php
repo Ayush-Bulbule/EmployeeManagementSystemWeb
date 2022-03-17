cc1940b598ac1b04d0254f3987b4854537d09946
Ayush Bulbule <ayushbulbule24@gmail.com>
2022-03-17 23:53:16 +0530

    Added responsiveness to login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../style/loginstyle.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/loginstyle.css">
 <div class="h-100vh">
     <nav>
         <!-- <div class="container-fluid d-flex align-items-center justify-content-center mb-0  py-3 shadow">
@@ -12,38 +12,39 @@
     <div class="container-fluid" style="background-color: #ecf0f4;">
 
         <div class="row pt-2">
-            <div class="col-md-7 col-sm-12 ps-lg-5 py-5">
+            <div class="col-lg-7 col-sm-12 ps-lg-5 py-lg-5">
                 <img src="<?php echo base_url(); ?>public/login-page-img.png" alt="Login Image" class="img-fluid  py-lg-4 ps-lg-5">
             </div>
-            <div class="col-md-5 col-sm-12 pt-4 pe-lg-5">
-                <div class="box p-4 py-5 ms-4 me-5 login-box shadow">
+            <div class="col-lg-5 col-sm-12 pe-lg-5">
+                <div class="box p-4 py-lg-5 ms-lg-4  login-box shadow">
                     <h3 class="text-center ">
                         Weclome to EMS
                     </h3>
+
                     <!-- Form -->
                     <form action="<?php echo base_url('login') ?>" method="POST">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
                             <div class="input-group with-icon">
                                 <input class="form-control rounded p-2" name="email" type="text" placeholder="Email" autocomplete=off>
-                                <i class="lar la-envelope" style="font-size: 1.5rem;"></i>
-                            </div>
-                            <small class="form-text text-danger"><?php echo form_error('email'); ?></small>
 
+                                <i class="fa fa-envelope" aria-hidden="true" style="font-size: 1.3rem;"></i>
+                            </div>
+                            <small class="text-danger form-text"><?php echo form_error('email') ?></small>
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
                             <div class="input-group with-icon">
                                 <input class="form-control rounded p-2" name="password" type="password" placeholder="Password" autocomplete=off>
-                                <i class="las la-unlock-alt" style="font-size: 1.5rem;"></i>
+                                <i class="fa fa-lock" aria-hidden="true" style="font-size:1.3rem"></i>
                             </div>
-                            <small class="form-text text-danger"><?php echo form_error('password'); ?></small>
+                            <small class="text-danger form-text"><?php echo form_error('password') ?></small>
                         </div>
                         <div class="mb-3 form-check">
-                            <a href="/" class="link-fp">Forgot Password?</a>
+                            <a href="/" class="link-fp text-primary">Forgot Password?</a>
                         </div>
-                        <button type="submit" class="btn login-btn p-3 w-100">Submit</button>
+                        <button type="submit" class="btn login-btn p-2 w-100">Submit</button>
                     </form>
                 </div>
             </div>
