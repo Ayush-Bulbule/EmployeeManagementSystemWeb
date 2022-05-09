3778216a95e95502d68b7d5fa733f35db4bce747
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-05-09 22:20:53 +0530

    added base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ems-app/';
+$config['base_url'] = 'http://localhost/projects/weh_ems/EmployeeManagementSystemWeb/';
 
 /*
 |--------------------------------------------------------------------------
